1e3f28d7081deea8d93df6ab2b94c8f109485749
chris <chris@LAPTOP-T9OFFMSV>
2019-08-29 16:13:14 -0400

    <I made a change>

--- HelloISM4300/Program.cs
+++ HelloISM4300/Program.cs
@@ -10,6 +10,10 @@ namespace HelloISM4300
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+            //This is a comment
         }
     }
 }
